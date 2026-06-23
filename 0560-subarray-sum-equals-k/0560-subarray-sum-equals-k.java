d3944b2980ff89a52d09b25cd56c543bdd857090
Yawar Hussain <146350030+yawar-00@users.noreply.github.com>
2026-06-23 15:15:23 +0530

    Time: 1110 ms (23.81%), Space: 48.4 MB (87.15%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,5 +1,10 @@
 class Solution {
-    static{ Solution warmup = new Solution(); for(int i = 0; i< 500; ++i){ warmup.subarraySum(new int[2],0); } }
+    static{ 
+        Solution warmup = new Solution(); 
+        for(int i = 0; i< 500; ++i){
+            warmup.subarraySum(new int[2],0); 
+        } 
+    }
     public int subarraySum(int[] nums, int k) {
         int count =0;
         for(int i =1;i<nums.length;i++){
