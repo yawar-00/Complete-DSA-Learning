7d13c2c5fa932431b0c18eb4226fdb8b1e8b2fbe
Yawar Hussain <146350030+yawar-00@users.noreply.github.com>
2026-06-29 11:37:28 +0530

    Time: 5 ms (61.84%), Space: 44.1 MB (94.82%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char arr[] =new char[26];
+        for(int i =0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
