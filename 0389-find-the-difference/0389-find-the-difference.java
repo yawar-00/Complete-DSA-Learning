6176ee4c5287b8e853a71131a625ccb240af76a6
Yawar Hussain <146350030+yawar-00@users.noreply.github.com>
2026-06-29 19:54:54 +0530

    Time: 3 ms (41.47%), Space: 42.9 MB (92.6%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -3,9 +3,9 @@ class Solution {
         int sum =0;
         for(int i=0;i<t.length();i++){
              sum+=t.charAt(i);
-        }
-        for(int i=0;i<s.length();i++){
-            sum-=s.charAt(i);
+             if(i<s.length()){
+                sum-=s.charAt(i);
+             }
         }
         return (char)sum;
         
