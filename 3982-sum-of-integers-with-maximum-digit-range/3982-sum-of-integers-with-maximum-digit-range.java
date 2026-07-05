7e6ec53deaaff6a0d5fc43b8c8b9dc995d33864b
Yawar Hussain <146350030+yawar-00@users.noreply.github.com>
2026-07-06 00:30:37 +0530

    Time: 4 ms (7.55%), Space: 46.6 MB (6.35%) - LeetHub

---
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxDigitRange(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max=Integer.MIN_VALUE;
+        for(int i:nums){
+            int minDigit=Integer.MAX_VALUE;
+            int maxDigit=Integer.MIN_VALUE;
+            int num=i;
+            while(num>0){
+                int n = num%10;
+                if(n>maxDigit){
+                    maxDigit=n;
+                }
+                if(n<minDigit){
+                    minDigit=n;
+                }
+                num/=10;
+            }
+            map.put(i,(maxDigit-minDigit));
+            max = Math.max(max,maxDigit-minDigit);
+        }
+        int ans = 0;
+        for(int i:nums){
+            if(map.get(i)==max){
+                ans+=i;
+            }
+        }
+        return ans;
+    }
+}
